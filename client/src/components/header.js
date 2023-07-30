797f41bebae1b128cf73f172bc1459e8866acf12
stuart teh <stuartteh1995@gmail.com>
2023-07-30 20:22:49 +1000

    debug

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,56 +1,23 @@
-// import React, { Component } from 'react';
-// import { Menu } from 'semantic-ui-react';
-// import { Link } from 'react-router-dom';
-// import Logo from '../Images/Chat_Along_logo.jpg';
-// import '../Style/header.css';
-
-// export default class Header extends Component {
-//   render() {
-//     return (
-//       <Menu pointing secondary>
-//         <Menu.Item>
-//           <Link to="/">
-//             <img class="Img_logo" alt="logo" src={Logo} />
-//           </Link>
-//         </Menu.Item>
-
-//         <Menu.Menu position='right'>
-//           <Menu.Item name='logout' />
-//         </Menu.Menu>
-//       </Menu>
-//     );
-//   }
-// }
-
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import Logo from '../Images/Chat_Along_logo.jpg';
+import '../Style/header.css';
 
 export default class Header extends Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
   render() {
-    const { activeItem } = this.state
-
     return (
-      <Menu inverted>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          onClick={this.handleItemClick}
-        />
+      <Menu pointing secondary>
+        <Menu.Item>
+          <Link to="/">
+            <img class="Img_logo" alt="logo" src={Logo} />
+          </Link>
+        </Menu.Item>
+
+        <Menu.Menu position='right'>
+          <Menu.Item name='logout' />
+        </Menu.Menu>
       </Menu>
-    )
+    );
   }
 }
