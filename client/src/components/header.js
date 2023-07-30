f5064e978464db49407755232a1c01f471dd197d
stuart teh <stuartteh1995@gmail.com>
2023-07-30 20:16:02 +1000

    debug

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,56 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+// import React, { Component } from 'react';
+// import { Menu } from 'semantic-ui-react';
+// import { Link } from 'react-router-dom';
+// import Logo from '../Images/Chat_Along_logo.jpg';
+// import '../Style/header.css';
 
-const Header = ()=> {
+// export default class Header extends Component {
+//   render() {
+//     return (
+//       <Menu pointing secondary>
+//         <Menu.Item>
+//           <Link to="/">
+//             <img class="Img_logo" alt="logo" src={Logo} />
+//           </Link>
+//         </Menu.Item>
+
+//         <Menu.Menu position='right'>
+//           <Menu.Item name='logout' />
+//         </Menu.Menu>
+//       </Menu>
+//     );
+//   }
+// }
+
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+export default class Header extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
     return (
-        <>
-        <h1>I am a header</h1>
-        <h2 > <Link to = '/'>Logo</Link></h2>
-        </>
+      <Menu inverted>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
     )
+  }
 }
-
-export default Header;
