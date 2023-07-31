160a0c6a58e5f86c3b6334368c13429726517bc2
Karun Thiyagarajah <tknanthan@gmail.com>
2023-07-31 21:32:18 +1000

    post related files updated

--- client/src/components/AddPostButton.js
+++ client/src/components/AddPostButton.js
@@ -1,7 +1,117 @@
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// import { ADD_POST } from '../utils/mutations';
+
+// const AddPostButton = ({ topic }) => {
+//   const [showForm, setShowForm] = useState(false);
+//   const [formData, setFormData] = useState({
+//     username: '',
+//     heading: '',
+//     message: '',
+//   });
+//   const navigate = useNavigate();
+//   const [addPost] = useMutation(ADD_POST);
+
+//   const handleAddPostClick = () => {
+//     setShowForm(true);
+//   };
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+//   };
+
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       if (
+//         formData.username.trim() === '' ||
+//         formData.heading.trim() === '' ||
+//         formData.message.trim() === ''
+//       ) {
+//         alert('Please fill all the fields.');
+//         return;
+//       }
+//       await addPost({
+//         variables: {
+//           heading: formData.heading,
+//           message: formData.message,
+//           topic,
+//         },
+//       });
+//       setShowForm(false);
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+
+//   return (
+//     <>
+//       {showForm ? (
+//         <form onSubmit={handleFormSubmit}>
+//           <div>
+//             <label htmlFor="username">Username:</label>
+//             <input
+//               type="text"
+//               id="username"
+//               name="username"
+//               // value={formData.username}
+//               onChange={handleInputChange}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="heading">Post Heading:</label>
+//             <input
+//               type="text"
+//               id="heading"
+//               name="heading"
+//               value={formData.heading}
+//               onChange={handleInputChange}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="message">Description:</label>
+//             <textarea
+//               id="message"
+//               name="message"
+//               value={formData.message}
+//               onChange={handleInputChange}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <button type="submit">Add Post</button>
+//           </div>
+//         </form>
+//       ) : (
+//         <div>
+//           <label htmlFor="topic">Topic:</label>
+//           <input
+//             type="text"
+//             id="topic"
+//             value={topic}
+//             onChange={(e) => {
+//               navigate(`/post/${e.target.value}`);
+//             }}
+//           />
+//           <button onClick={handleAddPostClick}>Add a Post</button>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default AddPostButton;
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_POST } from '../utils/mutations';
+import '../Style/AddPostButton.css';
+import { Card, Form, Icon } from 'semantic-ui-react';
 
 const AddPostButton = ({ topic }) => {
   const [showForm, setShowForm] = useState(false);
@@ -49,59 +159,67 @@ const AddPostButton = ({ topic }) => {
   return (
     <>
       {showForm ? (
-        <form onSubmit={handleFormSubmit}>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="heading">Post Heading:</label>
-            <input
-              type="text"
-              id="heading"
-              name="heading"
-              value={formData.heading}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Description:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <button type="submit">Add Post</button>
-          </div>
-        </form>
-      ) : (
-        <div>
-          <label htmlFor="topic">Topic:</label>
-          <input
-            type="text"
-            id="topic"
-            value={topic}
-            onChange={(e) => {
-              navigate(`/post/${e.target.value}`);
-            }}
-          />
-          <button onClick={handleAddPostClick}>Add a Post</button>
+        <div className="AddPostContainer">
+          <Card className="AddPostCard custom-style">
+            <Card.Content>
+              <Card.Header>Post to {topic}.</Card.Header>
+            </Card.Content>
+
+            <Card.Content>
+              <Form onSubmit={handleFormSubmit}>
+                <Form.Input
+                  fluid
+                  label="Title:"
+                  placeholder="Title"
+                  className="input-field"
+                  type="text"
+                  id="heading"
+                  name="heading"
+                  value={formData.heading}
+                  onChange={handleInputChange}
+                  required
+                />
+                <Form.Input
+                  fluid
+                  label="Username:"
+                  placeholder="Username"
+                  className="input-field"
+                  type="text"
+                  id="username"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  required
+                />
+                <Form.TextArea
+                  fluid
+                  label="Description:"
+                  placeholder="Description"
+                  className="input-field"
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  required
+                />
+
+                <Form.Button color="orange">Submit</Form.Button>
+              </Form>
+            </Card.Content>
+          </Card>
         </div>
+      ) : (
+        <Icon
+          onClick={handleAddPostClick}
+          name="plus circle"
+          size="big"
+          color="orange"
+          className="custom-icon"
+        ></Icon>
       )}
     </>
   );
 };
 
 export default AddPostButton;
+
--- client/src/components/postcontent.js
+++ client/src/components/postcontent.js
@@ -1,16 +1,160 @@
 
 
+// import React, { useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useQuery, useMutation} from '@apollo/client';
+// import { QUERY_POST_BY_ID } from '../utils/queries';
+// import {UPDATE_POST, DELETE_POST, ADD_RESPONSE} from '../utils/mutations';
+
+// const PostContent = () => {
+//   const { id } = useParams();  
+//   const { loading, data } = useQuery(QUERY_POST_BY_ID, {      
+//     variables: { postId: id },
+//   });     
+
+//   const [showResponseForm, setShowResponseForm] = useState(false);
+//   const [showUpdateForm, setShowUpdateForm] = useState(false);
+//   const [userName, setUserName] = useState('');
+//   const [message, setMessage] = useState('');
+//   const [updatedContent, setUpdatedContent] = useState('');
+
+//   const post = data?.post;
+
+//   const addResponse = useMutation(ADD_RESPONSE);
+//   const updatePost = useMutation(UPDATE_POST);
+
+
+//   const handleResponseClick = () => {
+//     setShowResponseForm(true);
+//     setShowUpdateForm(false);
+//   };
+
+//   const handleUpdateClick = () => {
+//     setShowUpdateForm(true);
+//     setShowResponseForm(false);
+//   };
+
+//   const handleResponseFormSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       if (userName.trim() === '' || message.trim() === '') {
+//         alert('Please fill all the fields.');
+//         return;
+//       }
+
+//       await addResponse({
+//         variables: {
+//           postId: id,
+//           message: `${userName}: ${message}`,
+//         },
+//       });
+
+//       setShowResponseForm(false);
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+
+//   const handleUpdateFormSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       if (updatedContent.trim() === '') {
+//         alert('Please enter updated content.');
+//         return;
+//       }
+
+//       await updatePost({
+//         variables: {
+//           postId: id,
+//           message: updatedContent,
+//         },
+//       });
+
+//       setShowUpdateForm(false);
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <>
+//       <h3>Post Content for {post?.heading}</h3>
+//       <p>{post?.message}</p>
+//       {!showResponseForm && !showUpdateForm && (
+//         <>
+//           <button onClick={handleResponseClick}>Response</button>
+//           <button onClick={handleUpdateClick}>Update Post</button>
+//         </>
+//       )}
+//       {showResponseForm && !showUpdateForm && (
+//         <form onSubmit={handleResponseFormSubmit}>
+//           <div>
+//             <label htmlFor="userName">User Name:</label>
+//             <input
+//               type="text"
+//               id="userName"
+//               name="userName"
+//               value={userName}
+//               onChange={(e) => setUserName(e.target.value)}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="message">Message:</label>
+//             <input
+//               type="text"
+//               id="message"
+//               name="message"
+//               value={message}
+//               onChange={(e) => setMessage(e.target.value)}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <button type="submit">Submit Reaction</button>
+//           </div>
+//         </form>
+//       )}
+//       {!showResponseForm && showUpdateForm && (
+//         <form onSubmit={handleUpdateFormSubmit}>
+//           <div>
+//             <label htmlFor="updatedContent">Updated Content:</label>
+//             <textarea
+//               id="updatedContent"
+//               name="updatedContent"
+//               value={message}
+//               onChange={(e) => setUpdatedContent(e.target.value)}
+//               required
+//             />
+//           </div>
+//           <div>
+//             <button type="submit">Update Post</button>
+//           </div>
+//         </form>
+//       )}
+//     </>
+//   );
+// };
+
+// export default PostContent;
+
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery, useMutation} from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_POST_BY_ID } from '../utils/queries';
-import {UPDATE_POST, DELETE_POST, ADD_RESPONSE} from '../utils/mutations';
+import { UPDATE_POST, ADD_RESPONSE } from '../utils/mutations';
+import { Segment, Button, Comment, Form, Header, Input } from 'semantic-ui-react';
+import '../Style/PostContent.css';
 
 const PostContent = () => {
-  const { id } = useParams();  
-  const { loading, data } = useQuery(QUERY_POST_BY_ID, {      
+  const { id } = useParams();
+  const { loading, data } = useQuery(QUERY_POST_BY_ID, {
     variables: { postId: id },
-  });     
+  });
 
   const [showResponseForm, setShowResponseForm] = useState(false);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
@@ -18,11 +162,10 @@ const PostContent = () => {
   const [message, setMessage] = useState('');
   const [updatedContent, setUpdatedContent] = useState('');
 
-  const post = data?.post;
+  const postContent = data?.post;
 
-  const [addResponse] = useMutation(ADD_RESPONSE);
-  const [updatePost] = useMutation(UPDATE_POST);
-
+  const addResponse = useMutation(ADD_RESPONSE);
+  const updatePost = useMutation(UPDATE_POST);
 
   const handleResponseClick = () => {
     setShowResponseForm(true);
@@ -82,60 +225,47 @@ const PostContent = () => {
 
   return (
     <>
-      <h3>Post Content for {post?.heading}</h3>
-      <p>{post?.message}</p>
-      {!showResponseForm && !showUpdateForm && (
-        <>
-          <button onClick={handleResponseClick}>Response</button>
-          <button onClick={handleUpdateClick}>Update Post</button>
-        </>
-      )}
-      {showResponseForm && !showUpdateForm && (
-        <form onSubmit={handleResponseFormSubmit}>
-          <div>
-            <label htmlFor="userName">User Name:</label>
-            <input
-              type="text"
-              id="userName"
-              name="userName"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
-            <input
-              type="text"
-              id="message"
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <button type="submit">Submit Reaction</button>
-          </div>
-        </form>
-      )}
-      {!showResponseForm && showUpdateForm && (
-        <form onSubmit={handleUpdateFormSubmit}>
-          <div>
-            <label htmlFor="updatedContent">Updated Content:</label>
-            <textarea
-              id="updatedContent"
-              name="updatedContent"
-              value={updatedContent}
-              onChange={(e) => setUpdatedContent(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <button type="submit">Update Post</button>
-          </div>
-        </form>
-      )}
+      <div className="PostContainer">
+        <h1 className="PostTitle">{postContent?.heading}</h1>
+        <p className="PostUsername">By: {postContent?.username}</p>
+        <h2>{postContent?.message}</h2>
+      </div>
+
+      <Comment.Group minimal>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
+
+        {/* Render comments here */}
+
+        <div className="CommentContainer">
+          <Form onSubmit={handleResponseFormSubmit}>
+            <Form.Field>
+              <Input
+                placeholder="Comment"
+                type="text"
+                id="message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                placeholder="Username"
+                type="text"
+                id="userName"
+                name="userName"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                required
+              />
+            </Form.Field>
+            <Button content="Add Reply" labelPosition="left" icon="edit" primary />
+          </Form>
+        </div>
+      </Comment.Group>
     </>
   );
 };
